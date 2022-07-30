e5d0023c418ef57aa3c82575968e15f861749ecc
Dan <dania.demidov@yandex.ru>
2022-07-30 18:17:07 +0300

    Some activation functions

--- net.py
+++ net.py
@@ -1,6 +1,37 @@
 import numpy as np
 
 
+class Softmax:
+    def __init__(self):
+        self.inputs = None
+        self.outputs = None
+
+    def forward(self, inputs, normalize_input=False):
+        self.inputs = inputs.copy()
+
+        if normalize_input:
+            self.inputs -= np.max(self.inputs)
+
+        self.outputs = np.exp(self.inputs) / np.sum(np.exp(self.inputs))
+        return self.outputs
+
+
+class ReLU:
+    def __init__(self):
+        self.inputs = None
+        self.outputs = None
+
+        self.d_inputs = None
+
+    def forward(self, inputs):
+        self.inputs = inputs.copy()
+        self.outputs = np.maximum(0, self.inputs)
+        return self.outputs
+
+    def backward(self, gradient):
+        self.d_inputs = (self.outputs > 0) * gradient
+
+
 class DenseLayer:
     # NOTE: training in BATHES in NOT SUPPORTED
 
@@ -27,7 +58,7 @@ class DenseLayer:
             gradient = np.ones(self.n_neurons)
 
         self.d_inputs = np.dot(gradient, self.weights)
-        self.d_weights = np.dot(self.inputs.T, gradient).T
+        self.d_weights = np.dot(self.inputs.T, np.reshape(gradient, (1, self.n_neurons))).T
         self.d_biases = gradient.copy()
 
     def tweak(self, rate: float = 1):
@@ -36,10 +67,23 @@ class DenseLayer:
 
 
 if __name__ == '__main__':
-    layer = DenseLayer(3, 1)
-    layer.forward([0.9, 0.1, 0.043])
+    test_input = np.random.random(9)
+
+    layer = DenseLayer(9, 9)
+    layer.forward(test_input)
     print(layer.outputs)
+
     layer.backward()
     layer.tweak(rate=1/2)
-    layer.forward([0.9, 0.1, 0.043])
+
+    layer.forward(test_input)
     print(layer.outputs)
+
+    act1 = Softmax()
+    act2 = Softmax()
+
+    act1.forward(layer.outputs)
+    act2.forward(layer.outputs, normalize_input=True)
+
+    print(act1.outputs)
+    print(act2.outputs)
